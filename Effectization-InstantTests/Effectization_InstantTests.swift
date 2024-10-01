4514098a4ec5ef577bf9ea7f4a6b60cf9ff7c797
adibkn1 <adibkn1@gmail.com>
2024-10-01 17:16:52 +0530

    Initial Commit

---
+++ Effectization-InstantTests/Effectization_InstantTests.swift
@@ -0,0 +1,17 @@
+//
+//  Effectization_InstantTests.swift
+//  Effectization-InstantTests
+//
+//  Created by Aditya Agarwal on 01/10/24.
+//
+
+import Testing
+@testable import Effectization_Instant
+
+struct Effectization_InstantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
