1e9f55121f7b7440286f8ab99570c242f9d1b1f9
adibkn1 <adibkn1@gmail.com>
2024-12-07 14:28:48 +0530

    a

---
+++ ARClip1/ARClip1App.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct ARClip1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ARContentView()  // AR Content view we will create
+        }
+    }
+}
