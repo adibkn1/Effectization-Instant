858c9c50ea4bd7420362923e20789954ed1186a4
adibkn1 <adibkn1@gmail.com>
2025-06-03 23:35:34 +0530

    a

--- Effectization-Instant/AppDelegate.swift
+++ Effectization-Instant/AppDelegate.swift
@@ -13,6 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Initialize analytics
+        AnalyticsManager.shared.initialize()
+        
         // Override point for customization after application launch.
         return true
     }
